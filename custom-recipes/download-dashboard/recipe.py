c70e17ed15f2d5462f306a3e97cc095963b16f0d
admin <neba.nfonsang@dataiku.com>
2023-05-17 15:06:40 -0600

    Edited files

--- custom-recipes/download-dashboard/recipe.py
+++ custom-recipes/download-dashboard/recipe.py
@@ -25,7 +25,7 @@ server_url = get_recipe_config()["server_url"]
 api_version = get_recipe_config()["api_version"]
 site_id = get_recipe_config()["site_id"]
 view_id = get_recipe_config()["view_id"]
-filter = get_recipe_config().get("filter", "")
+filter = get_recipe_config().get("filter", {})
 
 # filter parameter keys and values
 filter_keys = list(filter.keys())
