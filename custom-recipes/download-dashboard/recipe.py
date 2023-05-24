420009f4812dbcae5744bfbe976c74587336c835
admin <neba.nfonsang@dataiku.com>
2023-05-23 22:22:10 -0600

    Edited files

--- custom-recipes/download-dashboard/recipe.py
+++ custom-recipes/download-dashboard/recipe.py
@@ -142,26 +142,3 @@ with server.auth.sign_in(tableau_auth):
                 #write the image of the view
                 filename_img = view.name + "_" + current_time + ".png"
                 output_folder.upload_stream(filename_img, view.image)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
