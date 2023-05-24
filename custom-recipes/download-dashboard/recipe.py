397d9d33eba6a84b933c4df7f1cc6427280e0da2
admin <neba.nfonsang@dataiku.com>
2023-05-23 21:11:38 -0600

    Edited files

--- custom-recipes/download-dashboard/recipe.py
+++ custom-recipes/download-dashboard/recipe.py
@@ -74,10 +74,8 @@ server.version = api_version
 ## If api version is not set, the default api version will be used which canb be obtained using server.version
 
 
-# get the current time to be used as part of file names
 current_time = datetime.datetime.now()
 current_time = current_time.strftime("%m_%d_%Y_%H_%M_%S")
-
 
 # download and write csv data to a managed folder
 with server.auth.sign_in(tableau_auth):
@@ -85,34 +83,67 @@ with server.auth.sign_in(tableau_auth):
         if view_id == view.id:
             # set the csv data request option
             csv_req_option = TSC.CSVRequestOptions(maxage=1)
-            # filter the view on some conditions
-            for k, v in filter_key_value_pairs:
-                csv_req_option.vf(k, v)
-            # create a view image
-            server.views.populate_csv(view, csv_req_option)
-            #write the image of the view
-            filename = view.name + "_" + current_time + ".csv"
-            output_folder.upload_stream(filename, view.csv)
-
-
-# download and write image to a managed folder
-with server.auth.sign_in(tableau_auth):
-    for view in TSC.Pager(server.views):
-        if view_id == view.id:
-            # set the image request option
+            # set the csv data request option
             image_req_option = TSC.ImageRequestOptions(
                         imageresolution=TSC.ImageRequestOptions.Resolution.High,
                         maxage=1)
+            
             # filter the view on some conditions
             for k, v in filter_key_value_pairs:
-                image_req_option.vf(k, v)
-
-            # create a view image
+                csv_req_option.vf(k, v) # csv
+                image_req_option.vf(k, v) # image
+                # create a view csv and image
+            server.views.populate_csv(view, csv_req_option)
             server.views.populate_image(view, image_req_option)
-            #write the image of the view
-            filename = view.name + "_" + current_time + ".png"
-            output_folder.upload_stream(filename, view.image)
+            
+            if filter_column:
+                # get the dataframe from the csv
+                bytes_ = b''.join(view.csv)
+                string = bytes_.decode('utf-8')
+                csv_reader = csv.reader(string.split('\n'), delimiter=',', quoting=csv.QUOTE_ALL)
+                data = [row for row in csv_reader]
+                df = pd.DataFrame(data[1:], columns=data[0])
+                df = df.dropna(subset=[filter_column], axis="rows")
+                # get unique values of column parameter from dataframe
+                col_values = df[filter_column].unique().tolist()
+                col_key_values = [(filter_column, i) for i in col_values]
 
+                # create key-value pairs for filtering
+                for k_v in col_key_values:
+                    filter_key_value_pairs.append(k_v)
+                    print("Filter: ", filter_key_value_pairs)                    
+                    # set the csv data request option
+                    csv_req_option = TSC.CSVRequestOptions(maxage=1)
+                    # set the csv data request option
+                    image_req_option = TSC.ImageRequestOptions(
+                        imageresolution=TSC.ImageRequestOptions.Resolution.High,
+                        maxage=1)
+                    
+                    for k, v in filter_key_value_pairs:
+                        csv_req_option.vf(k, v) # csv
+                        image_req_option.vf(k, v) # image
+                        # create a view csv and image
+                    server.views.populate_csv(view, csv_req_option)
+                    server.views.populate_image(view, image_req_option)
+
+                    #write the csv of the view
+                    filename_csv = view.name + "_" + k_v[1] + current_time + ".csv"
+                    folder.upload_stream(filename_csv, view.csv)
+
+                    #write the image of the view
+                    filename_csv = view.name + "_" + k_v[1] + current_time + ".png"
+                    folder.upload_stream(filename_csv, view.image)
+                    # remove the last col key value pair
+                    filter_key_value_pairs.remove(k_v)
+
+            else:
+                #write the csv of the view
+                filename_csv = view.name + "_" + current_time + ".csv"
+                folder.upload_stream(filename_csv, view.csv)
+
+                #write the image of the view
+                filename_csv = view.name + "_" + current_time + ".png"
+                folder.upload_stream(filename_csv, view.image)
 
 
 
