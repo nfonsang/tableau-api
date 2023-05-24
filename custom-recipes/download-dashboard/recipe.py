4ea6759464d98821fd2645d00a769f140612d4bb
admin <neba.nfonsang@dataiku.com>
2023-05-23 21:10:18 -0600

    Edited files

--- custom-recipes/download-dashboard/recipe.py
+++ custom-recipes/download-dashboard/recipe.py
@@ -30,7 +30,6 @@ filter = get_recipe_config().get("filter", {})
 range_filter = get_recipe_config().get("range_filter", {})
 filter_column = get_recipe_config().get("filter_column", {})
 
-
 # filter parameter keys and values
 filter_keys = list(filter.keys())
 filter_values = list(filter.values())
