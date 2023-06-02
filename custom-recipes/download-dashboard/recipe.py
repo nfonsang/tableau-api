0617c5b9ee30ad7e54b0ea69f23d8d48754bc1cf
admin <neba.nfonsang@dataiku.com>
2023-06-01 18:10:13 -0600

    Edited files

--- custom-recipes/download-dashboard/recipe.py
+++ custom-recipes/download-dashboard/recipe.py
@@ -30,7 +30,7 @@ filter = get_recipe_config().get("filter", {})
 range_filter = get_recipe_config().get("range_filter", {})
 filter_column = get_recipe_config().get("filter_column", "")
 
-# clear folder before downloading the dashboard(s)
+#clear folder before downloading the dashboard(s)
 clear_folder = get_recipe_config()["clear_folder"]
 if clear_folder:
     folder.clear()
